579cb5928c228328e1b52fab140e16f9c0b9bdb9
jvenkateshmca <jalla.venkatesh.mca@gmail.com>
2019-01-28 14:31:04 +0530

    new function

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        // nextInt() reads the next integer from the keyboard
+        int number = reader.nextInt();
+
+        // println() prints the following line to the output screen
+
+        System.out.println("Please enter the number :" + number);
+        System.out.println("You enter :" + number);
+
+    }
+	
+	public void getSquare(int x)
+	{
+		float sqr=x*x;
+		System.out.println("Square of the "+x+" is :" + sqr);
+	}
+}
